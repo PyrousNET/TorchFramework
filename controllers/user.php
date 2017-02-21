c987aa5471d30c67a9ceee24a1239901990c927c
Ben Payne <trixtur@gmail.com>
2017-02-21 09:55:40 -0700

    Added missing helper to user controller and only pass back useful information in get method of user controller.

--- controllers/user.php
+++ controllers/user.php
@@ -84,7 +84,13 @@ class user_controller extends abstract_controller {
 	 * get_method
 	 */
 	protected function get_method($params) {
-		var_dump($this->_memcache->get($this->_key . "_user"));
+		$user = (object)array();
+		$user->username = $this->_memcache->get($this->_key . "_user")->username;
+		$user->full_name = $this->_memcache->get($this->_key . "_user")->full_name;
+		$user->email = $this->_memcache->get($this->_key . "_user")->email;
+		$user->date_created = $this->_memcache->get($this->_key . "_user")->date_created;
+
+		$this->_response = $user;
 	}
 	// =======================================================
 
@@ -110,4 +116,33 @@ class user_controller extends abstract_controller {
 		}
 	}
 	// =======================================================
+
+	/*
+	 * get_random_string
+	 */
+	protected function get_random_string($valid_chars, $length)
+	{
+		// start with an empty random string
+		$random_string = "";
+
+		// count the number of chars in the valid chars string so we know how many choices we have
+		$num_valid_chars = strlen($valid_chars);
+
+		// repeat the steps until we've created a string of the right length
+		for ($i = 0; $i < $length; $i++)
+		{
+			// pick a random number from 1 up to the number of valid chars
+			$random_pick = mt_rand(1, $num_valid_chars);
+
+			// take the random character out of the string of valid chars
+			// subtract 1 from $random_pick because strings are indexed starting at 0, and we started picking at 1
+			$random_char = $valid_chars[$random_pick-1];
+
+			// add the randomly-chosen char onto the end of our string so far
+			$random_string .= $random_char;
+		}
+
+		// return our finished random string
+		return $random_string;
+	}
 }
